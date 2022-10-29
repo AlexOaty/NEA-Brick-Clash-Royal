b2bb0d7a0d2b151653d2e90370ddcf1d7ef94afc
Alex <Alex@LAPTOP-2V8AEIIH>
2022-10-29 16:02:40 +0100

    Add project files.

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void Login()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CreateAccount()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+      
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Register()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
