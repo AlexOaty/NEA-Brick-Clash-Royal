01f655ce6e0a9e5ba1dd1af362d41119b70905da
Alex <alexotwayao@gmail.com>
2022-10-29 20:30:13 +0100

    Register Inputs

--- Assets/Scripts/LoginRegister.cs
+++ Assets/Scripts/LoginRegister.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginRegister : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    string Username;
+    string Password;
+
+    public void ReadUsername(string U)
     {
-        
+        Username = U;
+        Debug.Log(Username);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReadPassword(string P)
     {
-        
+        Password = P;
+        Debug.Log(Password);
+    }
+
+    public void Register()
+    {
+        Debug.Log($"Username: {Username}\nPassword: {Password}");
     }
 }
