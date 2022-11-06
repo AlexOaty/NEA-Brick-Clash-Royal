b7938ed451aa3283ae090bd9d301650aadedbd8a
AlexOaty <alexotwayao@gmail.com>
2022-11-06 17:53:52 +0000

    Follow path

--- Assets/Scripts/UnitBehaviour.cs
+++ Assets/Scripts/UnitBehaviour.cs
@@ -6,15 +6,40 @@ using UnityEngine;
 public class UnitBehaviour
 {
 
-    private GameObject Unit;
+    protected GameObject Unit;
+    protected GameObject PathToFollow;
+    protected Rigidbody2D rb;
+    protected float speed;
+    GameObject[] Paths;
 
-    public UnitBehaviour(GameObject Unit)
+    public UnitBehaviour(GameObject Unit, Rigidbody2D rb, float speed)
     {
         this.Unit = Unit;
+        this.rb = rb;
+        this.speed = speed;
     }
 
-    public void Move()
+    public void FollowPath()
     {
-        Unit.transform.position = new Vector3(0f, 0f, 0f);
+        float distance = Vector3.Distance(Unit.transform.position, PathToFollow.transform.position);
+        Vector3 direction = PathToFollow.transform.position - Unit.transform.position;
+        rb.AddForce(direction * distance * speed);
+    }
+
+    public GameObject FindPath()
+    {
+        Paths = GameObject.FindGameObjectsWithTag("Path");
+        float PathDistance0 = Vector3.Distance(Unit.transform.position, Paths[0].transform.position);
+        float PathDistance1 = Vector3.Distance(Unit.transform.position, Paths[1].transform.position);
+        if (PathDistance0 < PathDistance1)
+        {
+            PathToFollow = Paths[0];
+            return Paths[0];
+        }
+        else
+        {
+            PathToFollow = Paths[1];
+            return Paths[1];
+        }
     }
 }
--- Assets/Scripts/UnitMovement.cs
+++ Assets/Scripts/UnitMovement.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UnitMovement : MonoBehaviour
 {
     UnitBehaviour mUnit;
     GameObject Unit;
+    GameObject Path;
+    Rigidbody2D Rigidbody;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
         Unit = gameObject;
-        mUnit = new UnitBehaviour(Unit);
-        mUnit.Move();
+        Rigidbody = GetComponent<Rigidbody2D>();
+        mUnit = new UnitBehaviour(Unit, Rigidbody, speed);
+        Path = mUnit.FindPath();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(Unit.transform.position, Path.transform.position) >= 0.1)
+        {
+            mUnit.FollowPath();
+        }
     }
 }
