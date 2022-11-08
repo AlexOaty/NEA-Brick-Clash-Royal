be1e33e9b38f4854b9ea380cb1e07fb18341cd54
Alex OTWAY <16ao0630@pupil.uvhs.org.uk>
2022-11-08 15:29:13 +0000

    Path Following Progress

--- Assets/Scripts/UnitBehaviour.cs
+++ Assets/Scripts/UnitBehaviour.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class UnitBehaviour
 {
@@ -10,20 +11,57 @@ public class UnitBehaviour
     protected GameObject PathToFollow;
     protected Rigidbody2D rb;
     protected float speed;
+    protected bool IsEnemy;
     GameObject[] Paths;
+    GameObject[] PathBounds;
+    GameObject Bottom;
+    GameObject Top;
+    bool OnPath;
 
-    public UnitBehaviour(GameObject Unit, Rigidbody2D rb, float speed)
+    public UnitBehaviour(GameObject Unit, Rigidbody2D rb, float speed, bool IsEnemy)
     {
         this.Unit = Unit;
         this.rb = rb;
         this.speed = speed;
+        this.IsEnemy = IsEnemy;
     }
 
     public void FollowPath()
     {
-        float distance = Vector3.Distance(Unit.transform.position, PathToFollow.transform.position);
-        Vector3 direction = PathToFollow.transform.position - Unit.transform.position;
-        rb.AddForce(direction * distance * speed);
+        float distance;
+        Vector3 direction;
+        GameObject target;
+        if (!IsEnemy)
+            target = Bottom;
+        else
+            target = Top;
+        distance = Vector3.Distance(Unit.transform.position, target.transform.position);
+        if(distance > 0.1)
+        {
+            direction = target.transform.position - Unit.transform.position;
+            rb.AddForce(direction * distance * speed);
+        }
+        else
+        {
+            if (!IsEnemy)
+                target = Top;
+            else
+                target = Bottom;
+            distance = Vector3.Distance(Unit.transform.position, target.transform.position);
+            direction = target.transform.position - Unit.transform.position;
+            rb.AddForce(direction * distance * speed);
+        }
+        //if (OnPath)
+        //{
+        //    if (!IsEnemy)
+        //        target = Top;
+        //    else
+        //        target = Bottom;
+        //    distance = Vector3.Distance(Unit.transform.position, target.transform.position);
+        //    direction = target.transform.position - Unit.transform.position;
+        //    rb.AddForce(direction * distance * speed);
+        //}
+
     }
 
     public GameObject FindPath()
@@ -34,12 +72,29 @@ public class UnitBehaviour
         if (PathDistance0 < PathDistance1)
         {
             PathToFollow = Paths[0];
+            PathBounds = GameObject.FindGameObjectsWithTag("Left");
+            FindBounds();
             return Paths[0];
         }
         else
         {
             PathToFollow = Paths[1];
+            PathBounds = GameObject.FindGameObjectsWithTag("Right");
+            FindBounds();
             return Paths[1];
+        }
+    }
+    private void FindBounds()
+    {
+        if (PathBounds[0].transform.position.y < 0)
+        {
+            Top = PathBounds[1];
+            Bottom = PathBounds[0];
+        }
+        else
+        {
+            Top = PathBounds[0];
+            Bottom = PathBounds[1];
         }
     }
 }
--- Assets/Scripts/UnitMovement.cs
+++ Assets/Scripts/UnitMovement.cs
@@ -10,21 +10,19 @@ public class UnitMovement : MonoBehaviour
     GameObject Path;
     Rigidbody2D Rigidbody;
     public float speed;
+    public bool IsEnemy;
     // Start is called before the first frame update
     void Start()
     {
         Unit = gameObject;
         Rigidbody = GetComponent<Rigidbody2D>();
-        mUnit = new UnitBehaviour(Unit, Rigidbody, speed);
+        mUnit = new UnitBehaviour(Unit, Rigidbody, speed, IsEnemy);
         Path = mUnit.FindPath();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(Unit.transform.position, Path.transform.position) >= 0.1)
-        {
-            mUnit.FollowPath();
-        }
+        mUnit.FollowPath();
     }
 }
