3d64f128aa7ca9293bf0210c51ca9f33352bc886
Alex <alexotwayao@gmail.com>
2022-12-28 15:21:28 +0000

    Spawn Units on mouse

--- Assets/Scripts/UnitSpawner.cs
+++ Assets/Scripts/UnitSpawner.cs
@@ -10,8 +10,25 @@ using UnityEngine.WSA;
 public class UnitSpawner : MonoBehaviour
 {
     public GameObject Unit;
-    public void SpawnUnit()
+    public bool isActive = false;
+    void Update()
     {
-        Instantiate(Unit, new Vector3(-0.069f, -0.338f, 0), Quaternion.identity);
+        if (isActive)
+            Invoke("Spawn", 0f);
+    }
+
+    public void Activate()
+    {
+        isActive = true;
+    }
+
+    public void Spawn()
+    {
+        Vector3 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(Unit, new Vector3(MousePosition.x, MousePosition.y, 0f), Quaternion.identity);
+            isActive = false;
+        }
     }
 }
