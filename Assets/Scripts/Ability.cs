f51dda2018ea3ca4f661a157e8b93423e7c29aad
Alex OTWAY <16ao0630@pupil.uvhs.org.uk>
2023-01-24 15:30:20 +0000

    Tree

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "Ability", menuName = "Ability")]
 
 public class Ability : ScriptableObject
 {
+    public int Location;
     public string attribute;
     public int change;
     public int cost;
     public bool unlocked;
+    public int left;
+    public int right;
+    public GameObject Button;
 }
--- Assets/Scripts/ResearchTree.cs
+++ Assets/Scripts/ResearchTree.cs
@@ -1,17 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ResearchTree : MonoBehaviour
 {
     public List<Ability> abilities;
-    Ability[] Tree;
+    GameObject[] Buttons;
 
     private void Start()
     {
-        for (int i = 0; i < abilities.Count; i++)
-        {
+        Buttons = GameObject.FindGameObjectsWithTag("Tree");
+    }
 
+    private void Update()
+    {
+        //foreach (var Button in Buttons)
+        //{
+        //    if (abilities[int.Parse(Button.name)].unlocked)
+        //    {
+        //        Button.SetActive(true);
+        //    }
+        //}
+    }
+
+    public void Purchase(int Location)
+    {
+        foreach (Ability ability in abilities)
+        {
+            if (ability.Location == Location)
+            {
+                abilities[ability.left].unlocked = true;
+                abilities[ability.right].unlocked = true;
+
+
+            }
         }
     }
+
+
+
+
 }
